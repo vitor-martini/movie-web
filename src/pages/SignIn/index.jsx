2fc985bc79bf564692e1bc132994dd9870fe936c
Vitor Martini <vitor-martini@outlook.com>
2024-05-27 23:39:23 -0300

    feat: sign in page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,36 @@
-import { Container } from "./styles";
+import { Container, Form, BackgroundImg } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { FiMail, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 export function SignIn() {
   return (
     <Container>
-      hello world
+      <Form>
+        <h1>Movie Collection</h1>
+        <p>Acompanhe tudo o que assistir</p>
+
+        <h2>Faça seu login</h2>
+        <Input 
+          icon={ FiMail }
+          label={ "E-mail" }
+          inputId={ "email" }/>
+
+        <Input
+          icon={ FiLock }
+          label={ "Senha" }
+          inputId={ "senha" }
+        />
+
+        <Button
+          title="Entrar"
+          type="Submit"
+        />
+
+        <Link to="/register"> Criar conta</Link>
+      </Form>
+      <BackgroundImg />
     </Container>
   );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,3 +1,49 @@
 import styled from "styled-components";
+import wallpaper from "../../assets/wallpaper.png";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  min-width: 500px;
+  padding: 0 136px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > h1 {
+    font-weight: 700;
+    font-size: 4.8rem;
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  > p {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  > h2 {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-size: 2.4rem;
+    margin: 4rem 0 3.2rem;
+  }
+
+  > button {
+    margin: 1.6rem 0 4.2rem;
+  }
+
+  > a {
+    text-align: center; 
+    color: ${({ theme }) => theme.COLORS.PINK };
+  }
+`;
+
+export const BackgroundImg = styled.div`
+  flex: 1;
+  background: url(${ wallpaper }) no-repeat center center;
+  background-size: cover;
+  filter: brightness(0.5);
+`;
