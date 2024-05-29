3d81c0c0684e4bbda7ef905aa73a53339466e2cb
Vitor Martini <vitor-martini@outlook.com>
2024-05-28 21:43:59 -0300

    feat: sign up interface

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,43 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 export function SignUp() {
   return (
     <Container>
-      hello world
+      <Form>
+        <h1>Movie Collection</h1>
+        <p>Acompanhe tudo o que assistir</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input
+          icon={FiUser}
+          label={"Nome"}
+          inputId={"nome"}
+        />
+
+        <Input
+          icon={FiMail}
+          label={"E-mail"}
+          inputId={"email"} />
+
+        <Input
+          icon={FiLock}
+          label={"Senha"}
+          inputId={"senha"}
+        />
+
+
+        <Button
+          title="Cadastrar"
+          type="Submit"
+        />
+
+        <Link to="/"> <FiArrowLeft size={20} />Voltar para o login</Link>
+      </Form>
     </Container>
   );
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,3 +1,49 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-content: center;
+`;
+
+export const Form = styled.form`
+  min-width: 500px;
+  padding: 0 136px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > h1 {
+    font-weight: 700;
+    font-size: 4.8rem;
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  > p {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  > h2 {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-size: 2.4rem;
+    margin: 4rem 0 3.2rem;
+  }
+
+  > button {
+    margin: 1.6rem 0 4.2rem;
+  }
+
+  > a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    color: ${({ theme }) => theme.COLORS.PINK};
+    
+    svg {
+      color: ${({ theme }) => theme.COLORS.PINK};
+    }
+  }
+`;
