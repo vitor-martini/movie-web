127bbc6a0e877c22b91f724daee975cfc5519ad0
Vitor Martini <vitor-martini@outlook.com>
2024-05-30 18:42:01 -0300

    feat: profile interface

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,46 @@
-import { Container } from "./styles";
+import { Container, Header, Form, ProfilePic } from "./styles";
+import { Link } from "react-router-dom";
+import { FiArrowLeft, FiCamera, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function Profile() {
   return (
     <Container>
-      hello world
+      <Header>
+        <Link to="/">
+          <FiArrowLeft size={ 20 }></FiArrowLeft> Voltar
+        </Link>
+      </Header>
+      <Form>
+        <ProfilePic>
+          <img src="https://github.com/vitor-martini.png" alt="Foto de Vitor Martini" />
+          <label htmlFor="profilePic">
+            <FiCamera size={ 20 }/>
+            <input type="file" id="profilePic" />
+          </label>
+        </ProfilePic>
+        <Input 
+          icon={FiUser}
+          label={"Usuário"}
+          inputId={"username"}/>
+        <Input
+          icon={FiMail}
+          label={"E-mail"}
+          inputId={"email"}
+        />
+        <Input
+          icon={FiLock}
+          label={"Senha atual"}
+          inputId={"currentPassword"}
+        />
+        <Input
+          icon={FiLock}
+          label={"Nova senha"}
+          inputId={"newPassword"}
+        />
+        <Button title={"Salvar"}/>
+      </Form>
     </Container>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,69 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+`;
+
+export const Header = styled.header`
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+  height: 20%;
+  display: flex;  
+  padding-left: 10%;
+
+  > a {
+    color: ${({ theme }) => theme.COLORS.PINK};
+    display: flex;
+    align-items: center;
+
+    svg {
+      color: ${({ theme }) => theme.COLORS.PINK};
+      margin-right: 0.8rem;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  max-width: 50rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  > button {
+    margin-top: 0.8rem;
+  }
+`;
+
+export const ProfilePic = styled.div`
+  position: relative;
+  margin: -124px auto 32px;
+  width: 186px;
+  height: 186px;
+
+  > img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  > label {
+    width: 48px;
+    height: 48px;
+    background-color: ${({ theme }) => theme.COLORS.PINK};
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+    cursor: pointer;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    }
+  }
+`;
