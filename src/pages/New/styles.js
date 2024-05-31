7908a6f1bb0e66b0900c388a07c38c41c9403fb2
Vitor Martini <vitor-martini@outlook.com>
2024-05-30 23:50:56 -0300

    fix: remove unused style

--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -16,16 +16,6 @@ export const Container = styled.div`
   }
 `;
 
-export const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  flex: 1; 
-  padding: 3.6rem 3.2rem;
-  width: 80%;
-  margin: 0 auto; 
-  overflow: hidden; 
-`;
-
 export const Form = styled.form`
   margin: 0 auto;
   width: 100%;
