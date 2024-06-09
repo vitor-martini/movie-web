aad250ad8c41ab8dc56ffcc623ff8ae72976271b
Vitor Martini <vitor-martini@outlook.com>
2024-06-09 12:21:07 -0300

    feat: add new movie

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -7,8 +7,33 @@ import { Button } from "../../components/Button";
 import { BackButton } from "../../components/BackButton";
 import MovieCoverPlaceholder from "../../assets/movie-cover-placeholder.png";
 import { FiCamera } from "react-icons/fi";
+import { useState } from "react";
+import { api } from "../../services/api";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  async function handleAdd() {
+    if(!title || !description) {
+      alert("Preencha todos os campos!");
+      return;
+    }
+
+    try {
+      await api.post("/movies", { title, description } );
+      setTitle("");
+      setDescription("");
+      alert("Filme cadastrado com sucesso!");
+    } catch(error) {
+      if(error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Ocorreu um erro ao cadastrar o filme.");
+      }
+    }
+  }
+
   return (
     <Container>
       <Header/>
@@ -28,16 +53,28 @@ export function New() {
               <Input
                 label={"Título"}
                 inputId={"titulo"}
+                onChange={e => setTitle(e.target.value)}
+                value={title}
               />
               <TextArea
                 label={"Descrição"}
                 inputId={"descricao"}
+                onChange={e => setDescription(e.target.value)}
+                value={description}
               />
             </MovieInfo>
           </Details>
           <Buttons>
-            <Button className="delete-movie" title={"Excluir filme"}/>
-            <Button title={"Salvar"}/>
+            <Button 
+              className="delete-movie" 
+              title={"Excluir filme"}
+              type="Button"
+            />
+            <Button 
+              title={"Cadastrar"}
+              onClick={handleAdd}
+              type="Button"
+            />
           </Buttons>
         </Form>
       </Main>
